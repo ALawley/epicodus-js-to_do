7181f61f088334bd52a1cf665923fe2bf6815b83
Anna Lawley and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-21 10:18:10 -0700

    add extensions of task class

--- app/to-do.ts
+++ app/to-do.ts
@@ -6,9 +6,36 @@ class Task {
   }
 }
 
+class HomeTask extends Task {}
+
+class HobbyTask extends Task {
+  constructor(public description: string){
+    super(description, "Low");
+  }
+}
+
+class WorkTask extends Task {
+  constructor(public dueDate: Date, public description: string, public priority: string) {
+    super(description, priority);
+  }
+}
+
 var tasks= [];
-tasks.push(new Task("Do the dishes.", "Medium"));
-tasks.push(new Task("Buy chocolate.", "High"));
-tasks.push(new Task("Wash the laundry.", "Low"));
-tasks[0].markDone();
+tasks.push(new HomeTask("Do the dishes.", "Medium"));
+tasks.push(new HomeTask("Buy chocolate.", "High"));
+tasks.push(new HomeTask("Wash the laundry.", "Low"));
+
+tasks.push(new HobbyTask("Practice origami."));
+tasks.push(new HobbyTask("Bake a pie."));
+
+var today = new Date();
+var tomorrow = new Date();
+tomorrow.setDate(today.getDate() + 1);
+var nextDay = new Date();
+nextDay.setDate(today.getDate() + 2);
+
+tasks.push(new WorkTask(today, "Update blog.", "High"));
+tasks.push(new WorkTask(today, "Go to meeting.", "Medium"));
+tasks.push(new WorkTask(today, "Clean ceiling.", "Low"));
+
 console.log(tasks);
