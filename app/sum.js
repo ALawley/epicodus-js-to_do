c8ba717acb7475e537fb87b07e9f5848e6c8a8bd
Anna Lawley and Joseph Karasek <student@epicodus.com>
2016-03-21 10:01:21 -0700

    add some practice files for typscript

---
+++ app/sum.js
@@ -0,0 +1,7 @@
+var findSum = function (first, second) {
+    var sum = first + second;
+    alert("The sum of your two numbers is: " + sum);
+};
+var number = parseInt(prompt('Please enter a number...'));
+var otherNumber = parseInt(prompt('Please enter another number...'));
+findSum(number, otherNumber);
